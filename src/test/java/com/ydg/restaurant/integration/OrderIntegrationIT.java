711029f90d070791662ad4105750410b3cded8ce
Yeliz Nur Kılıç <kilicyeliznur@gmail.com>
2026-01-09 01:59:44 +0300

    11. düzeltme

--- src/test/java/com/ydg/restaurant/integration/OrderIntegrationIT.java
+++ src/test/java/com/ydg/restaurant/integration/OrderIntegrationIT.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("integration")
-class OrderIntegrationTest {
+class OrderIntegrationIT {
 
     @Autowired private MockMvc mockMvc;
     @Autowired private TableRepository tableRepository;
