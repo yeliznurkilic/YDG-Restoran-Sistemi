f89f0ede4d39374eb9a53c3284cf6a0938ffe5d2
Yeliz Nur Kılıç <kilicyeliznur@gmail.com>
2026-01-09 01:19:37 +0300

    @test eklendi IT için

--- src/test/java/com/ydg/restaurant/integration/OrderPaymentIntegrationTest.java
+++ src/test/java/com/ydg/restaurant/integration/OrderPaymentIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class OrderPaymentIntegrationTest {
 
     @Autowired private MockMvc mockMvc;
--- src/test/java/com/ydg/restaurant/integration/ReservationIntegrationTest.java
+++ src/test/java/com/ydg/restaurant/integration/ReservationIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ReservationIntegrationTest {
 
     @Autowired private MockMvc mockMvc;
